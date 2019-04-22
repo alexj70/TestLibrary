a453eddc5fa12c06027cae713a4b8bbe679686b5
aleksandr_unitspace <aleksandr@unitspace.top>
2019-04-22 11:58:34 +0300

    Initial commit

---
+++ TestLibrarySDK/TestLibrarySDK/TestLibrarySDK.h
@@ -0,0 +1,19 @@
+//
+//  TestLibrarySDK.h
+//  TestLibrarySDK
+//
+//  Created by Aleksandr Zhovtyi on 4/22/19.
+//  Copyright © 2019 Aleksandr Zhovtyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestLibrarySDK.
+FOUNDATION_EXPORT double TestLibrarySDKVersionNumber;
+
+//! Project version string for TestLibrarySDK.
+FOUNDATION_EXPORT const unsigned char TestLibrarySDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestLibrarySDK/PublicHeader.h>
+
+
