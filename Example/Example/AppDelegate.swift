ac482323e2f26da4ba0f06aac3547871ba56c393
aleksandr_unitspace <aleksandr@unitspace.top>
2019-04-22 12:14:35 +0300

    adds

---
+++ Example/Example/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  Example
+//
+//  Created by Aleksandr Zhovtyi on 4/22/19.
+//  Copyright © 2019 Aleksandr Zhovtyi. All rights reserved.
+//
+
+import UIKit
+import TestLibrarySDK
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        
+        return true
+    }
+
+    
+
+}
+
---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Aleksandr Zhovtyi on 4/22/19.
+//  Copyright © 2019 Aleksandr Zhovtyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
